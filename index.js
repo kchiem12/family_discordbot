1c242da2fae4db3761bb0a446270bba445202168
Kenneth Chiem <kc696@cornell.edu>
2023-05-29 13:30:07 -0400

    add command handler in index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
 // import the discord bot token later
 
 const client = new Client({
@@ -11,9 +13,53 @@ const client = new Client({
   ],
 });
 
+client.commands = new Collection();
+
+// get commands from the commands folder
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  if (!command.data || !command.execute) {
+    console.log(`Command ${file} is missing data or execute!`);
+    continue;
+  }
+  client.commands.set(command.data.name, command);
+}
+
 // only once
 client.once(Events.ClientReady, (c) => {
   console.log("Ready! Logged in as " + c.user.tag);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) {
+    console.error("No command found for " + interaction.commandName);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (err) {
+    console.error(err);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
